0961e91c10d78ad26d67e59b083d0d299fdf848c
ABCganada <shinmg9712@gmail.com>
2023-04-11 13:32:51 +0900

    SPRING BOOT - java code,스프링 빈 등록(비정형화)

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{    //MemberRepository interface를 implement한다.
 
     private static Map<Long, Member> store = new HashMap<>();
