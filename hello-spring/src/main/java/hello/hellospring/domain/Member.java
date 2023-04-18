b6446d010becb729bad5eff15b5ec04c62c4ce0d
ABCganada <shinmg9712@gmail.com>
2023-04-18 22:05:38 +0900

    SPRING BOOT - JPA + 통합테스트

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+
+import javax.persistence.*;
+
+@Entity // 자바가 관리하는 엔티티
 public class Member {
 
+    @Id //primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // Id를 자동으로 생성해주는 전략
     private Long id;        //id 식별자. 고객이 정하는 id가 아닌 시스템이 저장하는 id
+
     private String name;    //이름
 
     public Long getId() {
--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -23,8 +23,10 @@ public class MemberServiceIntegrationTest {
         //Given
         Member member = new Member();
         member.setName("hello");
+
         //When
         Long saveId = memberService.join(member);
+
         //Then
         Member findMember = memberRepository.findById(saveId).get();
         assertEquals(member.getName(), findMember.getName());
@@ -35,8 +37,10 @@ public class MemberServiceIntegrationTest {
         //Given
         Member member1 = new Member();
         member1.setName("spring");
+
         Member member2 = new Member();
         member2.setName("spring");
+
         //When
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));//예외가 발생해야 한다.
